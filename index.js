fe0e8c8916b13a9beaf97ec728f30f5c39af1bbf
Geslain Dahan <geslain.dahan@keyrus.com>
2019-02-27 15:35:31 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    socket.on('subscribeToTimer', (interval) => {
+        console.log('client is subscribing to timer with interval ', interval);
+        setInterval(() => {
+            socket.emit('timer', new Date());
+        }, interval);
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message',msg);
+    });
+});
+
+http.listen(8000, function(){
+    console.log('listening on *:8000');
+});
