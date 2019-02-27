307ec2c300d824e2dff9b8695a933663eeec2681
Geslain Dahan <geslain.dahan@keyrus.com>
2019-02-27 18:05:30 +0100

    add chat history

--- index.js
+++ index.js
@@ -6,20 +6,24 @@ app.get('/', function(req, res){
     res.send("Bubble-server")
 });
 
+var chatHistory = []
+
 io.on('connection', function(socket){
+    console.log(JSON.stringify(chatHistory))
+
     console.log('a user connected');
+
+    socket.on('chat history', function(){
+        socket.emit('chat history', chatHistory)
+    });
+
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
 
-    socket.on('subscribeToTimer', (interval) => {
-        console.log('client is subscribing to timer with interval ', interval);
-        setInterval(() => {
-            socket.emit('timer', new Date());
-        }, interval);
-    });
 
     socket.on('chat message', (msg) => {
+        chatHistory.push(msg)
         console.log('message: ' + msg);
         io.emit('chat message',msg);
     });
