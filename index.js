503ef9b163491c77fd7694cf87919d922f1ec1e9
Geslain Dahan <geslain.dahan@keyrus.com>
2019-02-28 13:53:45 +0100

    add facebook support

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const io = require('socket.io')(http);
 const bodyParser = require('body-parser');
 const TelegramBot = require('node-telegram-bot-api');
 const R = require('ramda')
+const axios = require('axios');
+
 require('dotenv').config()
 
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // replace the value below with the Telegram token you receive from @BotFather
@@ -13,39 +16,74 @@ const token = process.env.TELEGRAM_TOKEN;
 const bot = new TelegramBot(token);
 bot.setWebHook(process.env.TELEGRAM_WEBHOOK);
 
-app.get('/', function(req, res){
+function updateMessage(msg, telegramId = null) {
+    const formattedMessage = `[${msg.username}]: ${msg.message}`
+    chatHistory.push(msg)
+
+    // Chat
+    io.emit('chat message', msg);
+
+    // Telegram
+    if (telegramId) {
+        telegramChatIds = R.uniq([...telegramChatIds, telegramId])
+
+        // Telegram
+        R.without([telegramId], telegramChatIds).map(id => {
+            bot.sendMessage(id, formattedMessage);
+        })
+    } else {
+        telegramChatIds.map(id => {
+            bot.sendMessage(id, formattedMessage);
+        })
+    }
+
+    // Facebook
+    axios.post(process.env.FACEBOOK_WEBHOOK, {
+        message: formattedMessage
+    })
+
+}
+
+app.get('/', function (req, res) {
     res.send("Bubble-server")
 });
 
-app.post('/webhook/telegram', function(req, res){
+app.post('/webhook/telegram', function (req, res) {
     bot.processUpdate(req.body);
     res.sendStatus(200);
 });
 
+app.post('/webhook/facebook', function (req, res) {
+    const body = req.body
+
+    const msg = {
+        username: body["first name"],
+        application: "facebook",
+        message: body["last user freeform input"]
+    }
+
+    updateMessage(msg)
+    res.sendStatus(200);
+});
+
+
 var chatHistory = []
 var telegramChatIds = []
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log('a user connected');
 
-    socket.on('chat history', function(){
+    socket.on('chat history', function () {
         socket.emit('chat history', chatHistory)
     });
 
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         console.log('user disconnected');
     });
 
 
     socket.on('chat message', (msg) => {
-        chatHistory.push(msg)
-        io.emit('chat message',msg);
-
-        // Telegram
-        telegramChatIds.map(id => {
-            bot.sendMessage(id, `[${msg.username}]: ${msg.message}`);
-        })
-
+        updateMessage(msg)
     });
 });
 
@@ -57,18 +95,10 @@ bot.on('message', (msg) => {
         message: msg.text
     }
 
-    chatHistory.push(message)
-    telegramChatIds = R.uniq([...telegramChatIds, msg.chat.id])
-
-    // Telegram
-    R.without([msg.chat.id],telegramChatIds).map(id => {
-        bot.sendMessage(id, `[${message.username}]: ${message.message}`);
-    })
-
-    io.emit('chat message',message);
+    updateMessage(message, msg.chat.id)
 });
 
 
-http.listen(process.env.PORT || 8000, function(){
-    console.log('listening on '+ (process.env.PORT || 8000));
+http.listen(process.env.PORT || 8000, function () {
+    console.log('listening on ' + (process.env.PORT || 8000));
 });
